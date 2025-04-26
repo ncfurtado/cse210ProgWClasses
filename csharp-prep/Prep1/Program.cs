8ce66a6aee0d63835da11c2fc17ef724616db5ac
ncfurtado <fur22004@byui.edu>
2025-04-26 16:26:21 -0600

    Finished Prep 1 and 2 Assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,8 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
-        int x = 12;
-        Console.WriteLine(x);
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+        string letter = "Z";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+             letter = "B";
+        }
+        else if (gradeNumber >= 80)
+        {
+             letter = "C";
+        }
+        else if (gradeNumber >= 70)
+        {
+             letter = "D";
+        }
+        else if (gradeNumber < 60)
+        {
+             letter = "F";
+        }
+
+        Console.WriteLine($"{letter}");
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass, sorry!");
+        }
     }
 }
