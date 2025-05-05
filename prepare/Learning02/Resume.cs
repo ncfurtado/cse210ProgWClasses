9a25a5f12c8ba75f255cd9a06097b9a42c86fb07
ncfurtado <fur22004@byui.edu>
2025-05-05 10:27:29 -0600

    Finished Job/Resume Learning2 Program!

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks.Dataflow;
+
+class Resume
+{
+    public string _name;
+    public  List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"{_name}");
+        Console.WriteLine($"Jobs: ");
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
